29b2880383c9c08a6f9ccbe82a49d1c828267869
Hayashi Masahiro <mhayashi1120@gmail.com>
2010-12-23 16:18:14 +0900

    done

--- src/ProgrammingHaskell/Q1.hs
+++ src/ProgrammingHaskell/Q1.hs
@@ -1,12 +1,24 @@
 module Q1 () where
 
+double n = n + n
+
 -- a1
+-- double . double $ 2
 
 -- a2
+sum2 [] = 0
+sum2 [x] = x
+sum2 (x:xs) = x + sum xs
 
 -- a3
+product2 [] = 1
+product2 (x:xs) = x * product xs
 
 -- a4
-
+qsort [] = []
+qsort (x:xs) = qsort larger ++ [x] ++ qsort smaller
+               where
+                 smaller = [a|a<-xs, a<=x]
+                 larger = [b|b<-xs, b>x]
 -- a5
-
+-- sort stability
